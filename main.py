15fd4f37994136eff9fad9b3a9b9630b35b8a6cf
BinRad <bradens000@citymail.cuny.edu>
2020-01-03 13:15:53 -0500

    SQL to create the databases is working properly. (added cross refrencing in tables)

--- main.py
+++ main.py
@@ -9,23 +9,25 @@ class Main:
         )
         self.cursor = mydb.cursor()
         print('Seems like everything is working properly!')
-        self.DeleteDB()
+        self.deleteDB()
         self.CreateTables()
 
-    def DeleteDB(self):
-        self.cursor.execute("DROP DATABASE 'chores'")
-        return
+    def deleteDB(self):
+        self.cursor.execute("DROP DATABASE `chores`")
 
     def CreateTables(self):
         self.cursor.execute("CREATE DATABASE chores")
-        self.cursor.execute("CREATE TABLE `chores`.`roomates` ("
+        self.cursor.execute("CREATE TABLE `chores`.`roommates` ("
                             "`name` VARCHAR(255) NOT NULL,"
                             "PRIMARY KEY (`name`),"
                             "UNIQUE INDEX `name_UNIQUE` (`name` ASC) VISIBLE) ")
         self.cursor.execute("CREATE TABLE `chores`.`tasks` ( "
                             "`task` VARCHAR(255) NOT NULL,"
+                            "`person` VARCHAR(255) NULL,"
                             "`frequency` VARCHAR(255) NULL,"
-                            "`catgory` VARCHAR(255) NULL,"
+                            "`category` VARCHAR(255) NULL,"
                             "PRIMARY KEY (`task`),"
-                            "UNIQUE INDEX `name_UNIQUE` (`task` ASC) VISIBLE) ")
-        return
+                            "UNIQUE INDEX `name_UNIQUE` (`task` ASC) VISIBLE, "
+                            "FOREIGN KEY(`person`) REFERENCES roommates(name) )")
+
+p1 = Main()
